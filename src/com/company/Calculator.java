0f32989b2aa732e5a8de3adfc2a09a795bc7ed09
katya <Demchenkoks90@gmail.com>
2022-01-12 11:52:43 +0200

    first commit

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,82 @@
+package com.company;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+//import sun.jvm.hotspot.debugger.UnalignedAddressException;
+
+public class Calculator {
+	private String operation;
+	private Scanner scanner;
+
+	public Calculator() {
+		this.scanner = new Scanner(System.in);
+	}
+// create public method calculate()
+	public void calculate(){
+		while (!"q".equals(operation)) {
+			try {
+				System.out.println(calculationStep());
+			} catch (InputMismatchException ex){
+				System.out.println("Wrong input data!");
+			}
+		}
+	}
+	// create private method calculate()
+	private double calculationStep() throws InputMismatchException {
+		System.out.println("enter first number:");
+		int firstNumber = Integer.parseInt(scanner.nextLine());
+		System.out.println("enter second number:");
+		int secondNumber = Integer.parseInt(scanner.nextLine());
+		System.out.println("enter operation: +, -, *, /, to perform or q if you want to quit: ");
+
+
+		operation = scanner.nextLine();
+
+		//to remove when other operations added
+//		if (!"-".equals(operation))  {
+//			throw new UnsupportedOperationException(String.format("The operation '%s' is not supported", operation));
+//		}
+
+		return performOperation(firstNumber, secondNumber);
+	}
+
+	private double performOperation(int firstNumber, int secondNumber) {
+		double result;
+
+		switch (operation){
+			case "+":
+				result = plus(firstNumber, secondNumber);
+				break;
+			case "-":
+				result = minus(firstNumber, secondNumber);
+				break;
+			case "*":
+				result = multiply(firstNumber, secondNumber);
+				break;
+			case "/":
+				result = division(firstNumber, secondNumber);
+				break;
+			default:
+				result = 0;
+				break;
+		}
+
+		return result;
+	}
+
+	private int plus(int numberOne, int numberTwo){
+		return numberOne + numberTwo;
+	}
+	private int minus(int numberOne, int numberTwo){
+		return numberOne - numberTwo;
+	}
+	private int multiply(int numberOne, int numberTwo){
+		return numberOne * numberTwo;
+	}
+	private double division(double numberOne, double numberTwo){
+		return numberOne / numberTwo;
+	}
+
+}
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+//      create object calculator
+        Calculator calculator = new Calculator();
+//      call method calculate()
+        calculator.calculate();
+    }
+}
