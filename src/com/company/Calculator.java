152a1bbbf5fc4d7850c6bdd6981877e9ef6381b9
katya <Demchenkoks90@gmail.com>
2022-01-13 11:34:16 +0200

    description

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -5,45 +5,54 @@ import java.util.Scanner;
 
 public class Calculator {
 	private String operation;
+    //	данные получаем в программу с помощью Scanner
+    //	Scanner class has scanner variable
 	private Scanner scanner;
 
 	public Calculator() {
 		this.scanner = new Scanner(System.in);
 	}
-// create public method calculate()
+    // create public method calculate() / Public -> entry point into the program, into the Calculator class
+    //	if method calculate() is 'void' -> it return nothing
 	public void calculate(){
+//		программа будет работать до того момента, пока то, что мы введем в переменную operation не станет равным "q"
 		while (!"q".equals(operation)) {
 			try {
+                //call method calculationStep() / if the word starts with a LOWERCASE letter and is followed by parentheses,
+				// so this is the name of a method or function
 				System.out.println(calculationStep());
-			} catch (InputMismatchException ex){
+			} catch (NumberFormatException ex){
 				System.out.println("Wrong input data!");
 			}
 		}
 	}
 	// create private method calculate()
-	private double calculationStep() throws InputMismatchException {
+    //	if method calculationStep() is 'double' -> it should return something
+	private double calculationStep() {
 		System.out.println("enter first number:");
 		int firstNumber = Integer.parseInt(scanner.nextLine());
 		System.out.println("enter second number:");
 		int secondNumber = Integer.parseInt(scanner.nextLine());
 		System.out.println("enter operation: +, -, *, /, to perform or q if you want to quit: ");
 
-
+        //  считываем строку scanner.nextLine()
 		operation = scanner.nextLine();
 
 		//to remove when other operations added
 //		if (!"-".equals(operation))  {
 //			throw new UnsupportedOperationException(String.format("The operation '%s' is not supported", operation));
 //		}
-
 		return performOperation(firstNumber, secondNumber);
 	}
 
+
+	// create private method performOperation()
 	private double performOperation(int firstNumber, int secondNumber) {
 		double result;
 
 		switch (operation){
 			case "+":
+                //	call methods plus()
 				result = plus(firstNumber, secondNumber);
 				break;
 			case "-":
@@ -62,7 +71,8 @@ public class Calculator {
 
 		return result;
 	}
-
+    // create private methods plus(), minus(), multiply(), division()
+    // if method plus() is 'int' -> it should return something
 	private int plus(int numberOne, int numberTwo){
 		return numberOne + numberTwo;
 	}
@@ -72,9 +82,7 @@ public class Calculator {
 	private int multiply(int numberOne, int numberTwo){
 		return numberOne * numberTwo;
 	}
-	private double division(double numberOne, double numberTwo){
-		return numberOne / numberTwo;
-	}
+	private double division(int numberOne, double numberTwo){ return numberOne / numberTwo; }
 
 }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//      create object calculator
+//      create new object calculator for Calculator class
         Calculator calculator = new Calculator();
 //      call method calculate()
         calculator.calculate();
