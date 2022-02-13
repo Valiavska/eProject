f6dea1faf55e2739e1f5a3a4aa3409d3407bd2a3
katya <Demchenkoks90@gmail.com>
2022-02-13 15:36:13 +0200

    test_stream

--- src/com/company/amazon_collection_hw5/PriceComparatorKeys1.java
+++ src/com/company/amazon_collection_hw5/PriceComparatorKeys1.java
@@ -6,6 +6,6 @@ public class PriceComparator implements Comparator<Product> {
 
     @Override
     public int compare(Product o1, Product o2) {
-        return o1.getPrice() - o2.getPrice();
+        return Integer.compare(o1.getPrice(), o2.getPrice());
     }
 }
